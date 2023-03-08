4a778d179017169403d61c3eb0d2da3357640cec
Richie <molunorichie@gmail.com>
2023-03-08 02:09:32 +0100

    add: express server setup - app.tx

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { NODE_ENV, PORT } from './config';
 
+function initMiddlewares(app: Application) {
+    NODE_ENV == 'dev' ? app.use(morgan('dev')) : null;
 
+    app.use(express.json());
+    app.use(cors());
+}
+
+function initExpressRouteHandler(app: Application) {}
+
+export const app: Application = express();
+
+export function startExpressServer() {
+    initMiddlewares(app);
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}...`);
+    });
+}
