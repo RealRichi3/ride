9bbda5f612a3920670246260eba0b4cbf45bb64d
Richie <molunorichie@gmail.com>
2023-03-09 21:56:53 +0100

    update path

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,30 @@
-import dotenv from "dotenv";
-import { NodeENV } from "./types";
+import dotenv from 'dotenv';
+import { NodeENV } from './types';
 
-dotenv.config({
-    path: `${__dirname}/.env.${process.env.NODE_ENV as NodeENV}`,
-});
+/**
+ * Set env config based on current node environment
+ * i.e
+ * if NODE_ENV = 'dev' use .env.dev file
+ * if NODE_ENV = 'prod' use .env.prod
+ * if NODE_ENV = 'test' use .env.prod
+ */
 
-import { connectToDatabase } from "./database";
-import { startExpressServer } from "./app";
+let path: string;
+const NODE_ENV = process.env.NODE_ENV as NodeENV;
+
+path = NODE_ENV ? `${__dirname}/.env.${NODE_ENV}` : `${__dirname}/.env`;
+dotenv.config({ path });
+
+import { connectToDatabase } from './database';
+import { startExpressServer } from './app';
 
 async function startServer() {
     try {
         await connectToDatabase();
-        
-        startExpressServer()
 
+        startExpressServer();
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
