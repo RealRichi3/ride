1c2685fd88b0f586d8cf4ab1dc2da458b5a5d73d
Richie <molunorichie@gmail.com>
2023-03-10 03:26:53 +0100

    update errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,57 +1,74 @@
 import { Request, Response, NextFunction } from 'express';
 import { Error } from 'mongoose';
-import { MongoError } from 'mongodb';
 import { MongoDuplicateKeyError } from '../types';
 import { BadRequestError, CustomAPIError, InternalServerError } from '../utils/errors';
 
+/**
+ * This function is used to handle all errors that occur in the application.
+ * It takes in an error object, and returns an appropriate error response to the client.
+ *
+ * @param {Error} err - the error object being handled.
+ * @param {Request} req - the request object.
+ * @param {Response} res - the response object.
+ * @param {NextFunction} next - the next function in the middleware chain.
+ *
+ * @returns {Response} - a response object containing the error message and status code.
+ */
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-    // NODE_ENV != 'test' ? console.log(err) : null;
+    // log the error to the console, but only if the environment is not "test"
+    process.env.NODE_ENV !== 'test' ? console.log(err) : null;
 
-    let error: CustomAPIError;
+    // create a variable to hold the custom error object
+    let error: CustomAPIError | undefined;
 
-    /** Handle Error based on it's type */
-    if (err instanceof MongoError) {
+    // check the type of error and create a custom error object for it
+    if (err.name === 'MongoServerError') {
+        // handle duplicate key errors
         const mongo_error = err as MongoDuplicateKeyError;
-        if (mongo_error.code == 11000) {
-            /** Handles Duplicate key error */
+        if (mongo_error.code === 11000) {
+            // get the key value causing the duplicate key error
             const error_key_value: string = mongo_error.keyValue?.email;
             const message = `${error_key_value || 'User'} already exists, please use another email`;
-
+            
             error = new BadRequestError(message);
         } else {
+            // handle all other Mongo errors
+
             error = new InternalServerError('An error occurred');
         }
     } else if (err instanceof Error.ValidationError) {
-        /** Handles Mongoose Schema Validation error */
-
+        // handle validation errors from Mongoose
         const error_messages = Object.values(err.errors);
         const message = error_messages.join(', ');
-
+        
         error = new InternalServerError(message);
-    } else if (err.name == 'TokenExpiredError') {
-        /** Handles TokenExpiredError, occurs when JWT auth token has Expired  */
+    } else if (err.name === 'TokenExpiredError') {
+        // handle expired JWT tokens
 
         error = new CustomAPIError('Token expired', 401);
-    } else if (err.name == 'JsonWebTokenError' && err.message == 'jwt malformed') {
-        /**
-         *  Occurs when JWT was provided but token doesn't match Regex for JWT tokens
-         *  It is most likely due to extra characters or missing in the token
-         */
+    } else if (err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
+        // handle malformed JWT tokens
 
         error = new CustomAPIError('Invalid authentication token', 401);
     } else if (err instanceof CustomAPIError) {
-        /** Handle other API errors */
-        
+        // handle all other custom API errors
+
         return res.status(err.statusCode).send({
             data: null,
             message: err.message,
         });
     }
 
-    /**
-     * If it passes as not being other errors, its an Internal server error
-     */
-    return res.status(500).send({ message: 'An error occured' });
+    // if there is a custom error object, return it to the client
+    if (error) {
+        return res.status(error.statusCode).send({
+            data: null,
+            message: error.message,
+        });
+    }
+
+    // if the error is not one of the specific types above, return a generic internal server error
+    return res.status(500).send({ message: 'An error occurred' });
 }
 
 export default errorHandler;
