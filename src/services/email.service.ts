413f621af8a2684604b2f8439c58e9061f2ab863
Richie <molunorichie@gmail.com>
2023-03-14 21:47:43 +0100

    add: sendEmail - email.service.ts

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,2 +1,36 @@
 import nodemailer from 'nodemailer';
-import * as config from '../config'
+import * as config from '../config';
+import { Email } from '../types';
+
+interface IMailOptions {
+    from?: string;
+    to: Email;
+    subject: string;
+    text: string;
+    html?: string;
+}
+
+export async function sendEmail(mailOptions: IMailOptions): Promise<void | Error> {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: config.EMAIL_HOST,
+            port: config.EMAIL_PORT,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user: config.EMAIL_HOST_ADDRESS,
+                clientId: config.OAUTH_CLIENT_ID,
+                clientSecret: config.OAUTH_CLIENT_SECRET,
+                refreshToken: config.OAUTH_REFRESH_TOKEN,
+                accessToken: config.OAUTH_ACCESS_TOKEN,
+            },
+        });
+
+        mailOptions.from = mailOptions.from ?? config.EMAIL_HOST_ADDRESS;
+
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
