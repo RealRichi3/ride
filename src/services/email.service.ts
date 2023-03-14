5711497812c684ce2601ec16d420943ff0c660d0
Richie <molunorichie@gmail.com>
2023-03-14 22:47:05 +0100

    fix types for mail options - email.service.ts

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,15 +2,23 @@ import nodemailer from 'nodemailer';
 import * as config from '../config';
 import { Email } from '../types';
 
-interface IMailOptions {
+/**
+ * Type for mailOptions
+ *
+ * @description
+ * 1. Make text and html optional
+ * 2. Make text required if html is not provided
+ * 3. Make html required if text is not provided
+ * 4. Make from optional
+ */
+interface IPartialMailOptions {
     from?: string;
     to: Email;
     subject: string;
-    text: string;
-    html?: string;
 }
+type TMailOptions = IPartialMailOptions & ({ text: string } | { html: string });
 
-export async function sendEmail(mailOptions: IMailOptions): Promise<void | Error> {
+export async function sendEmail(mailOptions: TMailOptions): Promise<void | Error> {
     try {
         const transporter = nodemailer.createTransport({
             host: config.EMAIL_HOST,
