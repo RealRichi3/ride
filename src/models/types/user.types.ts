163d76e62812cd96e94cbe95cb668e95c95e1804
Richie <molunorichie@gmail.com>
2023-03-14 23:23:20 +0100

    update emailType in user interface - user.types.ts

--- src/models/types/user.types.ts
+++ src/models/types/user.types.ts
@@ -1,11 +1,12 @@
 import { Types, Document } from 'mongoose';
 import { IPassword } from './password.types';
 import { IStatus } from './status.types';
+import { Email } from '../../types';
 
 interface IUser extends Document {
     firstname: string;
     lastname: string;
-    email: string;
+    email: Email;
     role: 'EndUser' | 'Admin' | 'SuperAdmin';
     googleId?: string;
     githubId?: string;
