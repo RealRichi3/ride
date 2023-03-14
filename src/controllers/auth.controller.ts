fbd51582e58d4bf7a66e5c6e826d1638717905e8
Richie <molunorichie@gmail.com>
2023-03-14 01:01:19 +0100

    add: basic flow for usersignup - auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,20 +1,43 @@
 import { Request, Response, NextFunction } from 'express';
 import { IUser, User } from '../models/user.model';
+import { BadRequestError } from '../utils/errors';
+import mongoose from 'mongoose';
+import { Password } from '../models/password.model';
 
 const userSignup = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
+    const { email, firstname, lastname, password, role } = req.body;
+    const user_info = { email, firstname, lastname, password, role };
 
-    const user: IUser = await User.create(req.body);
-    console.log(user);
+    // Check if user already exists
+    const existing_user: IUser | null = await User.findOne({ email });
+    if (existing_user) {
+        // Handle existing user
+    }
 
-    return res.status(200).send({
-        success: true,
+    // Create new user in session
+    let user: IUser | undefined;
+    const session = await mongoose.startSession();
+    await session.withTransaction(async () => {
+        user = (await User.create([user_info], { session }))[0];
+
+        await session.commitTransaction();
+        session.endSession();
+    });
+
+    if (!user) throw new BadRequestError('An error occurred');
+
+    await Password.create({ user: user._id, password });
+
+    // Get access token
+
+    // Send response
+    res.status(201).json({
+        status: 'success',
+        message: 'User created successfully',
         data: {
             user,
         },
     });
 };
 
-export {
-    userSignup
-}
+export { userSignup };
