9462fef114326c509796a9d323924641cf82e5e4
Richie <molunorichie@gmail.com>
2023-03-15 00:17:04 +0100

    add: response type for handleExistingUser

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -14,11 +14,15 @@ import { Email } from '../types';
  * @description Sends verification email and returns access token
  *
  * @param unverified_user  User with unverified email address
+ * @param res  Response object
+ * @param next  Next function
  *
  * @returns { access_token: string}
  */
-async function handleUnverifiedUser(unverified_user: IUser & { status: IStatus })
-    : Promise<{ access_token: string; }> {
+async function handleUnverifiedUser(
+    unverified_user: IUser & { status: IStatus }, res: Response)
+    : Promise<Response> {
+
     // Get verificateion code
     const { verification_code }: { verification_code?: number }
         = await getAuthCodes(unverified_user, 'verification');
@@ -33,7 +37,14 @@ async function handleUnverifiedUser(unverified_user: IUser & { status: IStatus }
     // Get access token
     const { access_token } = await getAuthTokens(unverified_user, 'access');
 
-    return { access_token };
+    return res.status(200).send({
+        status: 'success',
+        message: 'Verification code sent to user email',
+        data: {
+            user: unverified_user,
+            access_token,
+        },
+    });
 }
 
 /**
@@ -42,27 +53,20 @@ async function handleUnverifiedUser(unverified_user: IUser & { status: IStatus }
  * @description Handles existing user
  *
  * @param existing_user  Existing user
+ * @param res  Response object
  *
- * @returns {void}
+ * @returns {Response}
  * */
-function handleExistingUser(existing_user: IUser & { status: IStatus }) {
-    return async function (req: Request, res: Response, next: NextFunction) {
-        // Handle existing user
+async function handleExistingUser(
+    existing_user: IUser & { status: IStatus }, res: Response, next: NextFunction)
+    : Promise<Response | NextFunction> {
 
-        if (!existing_user.status.isVerified) {
-            // Handle unverified user
-            const { access_token } = await handleUnverifiedUser(existing_user)
-
-            res.status(200).json({
-                status: 'success',
-                message: 'User created successfully',
-                data: {
-                    user: existing_user,
-                    access_token,
-                },
-            });
-        } else next(new BadRequestError('User already exists, please verify your account'));
-    };
+    const response =
+        existing_user.status.isVerified
+            ? next(new BadRequestError('Email belongs to an existing user'))
+            : await handleUnverifiedUser(existing_user, res);
+    
+    return response as  Response | NextFunction;
 }
 
 /**
@@ -79,6 +83,8 @@ function handleExistingUser(existing_user: IUser & { status: IStatus }) {
  * @throws { BadRequestError } If user is not verified
  * 
  * @returns { user: IUser, access_token: string }
+ * 
+ * //TODO: Test admin and super admin signup
  * */
 const userSignup = async (req: Request, res: Response, next: NextFunction) => {
     const { email, firstname, lastname, password, role } = req.body;
@@ -88,8 +94,7 @@ const userSignup = async (req: Request, res: Response, next: NextFunction) => {
     const existing_user: IUser | null = await User.findOne({ email }).populate('status');
     if (existing_user) {
         // Handle existing user
-        await handleExistingUser(existing_user.toObject())(req, res, next);
-        return
+        return await handleExistingUser(existing_user.toObject(), res, next);
     }
 
     // Create new user in session
@@ -109,17 +114,34 @@ const userSignup = async (req: Request, res: Response, next: NextFunction) => {
     await Password.create({ user: user._id, password });
 
     // Get access token
-    const { access_token } = await handleUnverifiedUser(user.toObject());
-
-    // Send response
-    res.status(200).json({
-        status: 'success',
-        message: 'User created successfully',
-        data: {
-            user,
-            access_token
-        },
-    });
+    return await handleUnverifiedUser(user.toObject(), res);
 };
 
-export { userSignup };
+/**
+ * Resend verification email
+ * 
+ * @description Resends verification email to user
+ * 
+ * @param { email: string } | User email
+ * 
+ * @throws { BadRequestError } If user does not exist
+ * @throws { BadRequestError } If user's email is already verified
+ * 
+ * @returns { user: IUser, access_token: string }
+ */
+const resendVerificationEmail = async (req: Request, res: Response, next: NextFunction) => {
+    const email = req.params.email as Email;
+
+    // Get user
+    const user: IUser & { status: IStatus } | null = await User.findOne({ email }).populate('status');
+
+    // Check if user exists
+    if (!user) return next(new BadRequestError('User does not exist'));
+
+    // Check if user is unverified
+    user.status.isVerified
+        ? next(new BadRequestError("User's email already verified"))
+        : await handleUnverifiedUser(user, res);
+}
+
+export { userSignup, resendVerificationEmail };
